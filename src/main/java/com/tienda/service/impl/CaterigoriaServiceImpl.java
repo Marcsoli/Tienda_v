02b63c37753187b269005b5d2b41952b458618f1
Marcos Solis <Marcos Solis@192.168.0.29>
2023-10-20 21:46:47 -0600

    Semana 6 CRUD

--- src/main/java/com/tienda/service/impl/CaterigoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CaterigoriaServiceImpl.java
@@ -7,18 +7,40 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CaterigoriaServiceImpl implements CategoriaService{
 
     @Autowired
-    private CategoriaDao CategoriaDao;
+    private CategoriaDao categoriaDao;
     
     @Override
     public List<Categoria> getCategorias(boolean activo) {
-       var categorias = CategoriaDao.findAll();
+       var categorias = categoriaDao.findAll();
+       if (activo) {
+            categorias.removeIf(e -> !e.getActivo());
+        }
        
        return categorias;
+    }
+    
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
     
 }
